d7d4435b968767ab1ea4e6aedfe32994dc50dd98
Rebecca Chen <rebecca061996@gmail.com>
2021-06-02 11:10:32 -0400

    Move context logic out of app and into own file

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
   onLogout: () => {},
+  onLogin: (email, password) => {},
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggedInInformation === 'LOGGED_IN') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', 'LOGGED_IN');
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      isLoggedIn={isLoggedIn}
+      onLogout={logoutHandler}
+      onLogin={loginHandler}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
